3a52a5815fe4061ba981a6f3f4e5373ce286eefb
lublagg <lvblagg@gmail.com>
2023-04-04 14:15:14 -0400

    Co2 indicator always visible.

--- src/app/components/simulation/simulation-view.tsx
+++ src/app/components/simulation/simulation-view.tsx
@@ -27,12 +27,9 @@ export const SimulationView: React.FC<IProps> = ({input, output, isRunning, isFi
       <div className={css.ruler}/>
       <div className={css.terrarium}>
         <div className={css.terrariumBack}/>
-        {
-        ((!isRunning && !isFinished) || output.time === 0) &&
         <div className={css.co2Label}>
           <div className={css.co2LabelText}> <span>CO<sub>2</sub></span>: {t(input.co2amount)} </div>
         </div>
-        }
         <div className={css.terrariumFrontGlass}/>
         <div className={clsx(css.soil, getClass(water))}/>
         <div className={clsx(css.light,  getClass(light))}/>
