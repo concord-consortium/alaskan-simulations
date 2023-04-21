91abe16726665030c79d2a1ffe0dea4ac371ceaf
lublagg <lvblagg@gmail.com>
2023-04-21 13:34:36 -0400

    String not active even if same value as clicked.

--- plant-growth/src/app/components/hooks/use-translation.tsx
+++ plant-growth/src/app/components/hooks/use-translation.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useCallback} from "react";
+import React, {useState} from "react";
 import { translations } from "../translations";
 import clsx from "clsx";
 
@@ -8,52 +8,73 @@ interface IProps {
   isRunning: boolean;
 }
 
+interface ITranslationProps {
+  string: string,
+  readAloudMode: boolean,
+  isAudioPlaying: boolean,
+  setIsAudioPlaying: (bool: boolean) => void,
+  isRunning: boolean
+}
+
+const Translation = (props: ITranslationProps) => {
+  const [clicked, setClicked] = useState<boolean>(false);
+  const {string, readAloudMode, isRunning, isAudioPlaying, setIsAudioPlaying} = props;
+  const stringToRender = string === "CO2" ? <span>CO<sub>2</sub></span> : translations[string].string;
+
+  if (readAloudMode && "mp3" in translations[string]) {
+    const audio = new Audio(translations[string].mp3);
+
+    audio.addEventListener("playing", () => {
+      setIsAudioPlaying(true);
+      setClicked(true);
+    });
+
+    audio.addEventListener("ended", () => {
+      setIsAudioPlaying(false);
+      setClicked(false);
+    });
+
+    const handlePlay = () => {
+      if (!isDisabled && !isAudioPlaying) {
+        audio.load();
+        audio.play();
+      }
+    };
+
+    const isActive = isAudioPlaying && clicked;
+    const isDisabled = isRunning || (isAudioPlaying && !clicked);
+
+    const classes = {
+      [css.readAloud]: true,
+      [css.active]: isActive,
+      [css.disabled]: isDisabled || isAudioPlaying
+    };
+
+    return (
+      <div onClick={handlePlay}>
+        <span className={clsx(classes)}>{stringToRender}</span>
+      </div>
+    );
+  } else {
+    return <div>{stringToRender}</div>;
+  }
+}
+
 export const useTranslation = (props: IProps) => {
   const [readAloudMode, setReadAloudMode] = useState<boolean>(false);
   const [isAudioPlaying, setIsAudioPlaying] = useState<boolean>(false);
-  const [activeAudioId, setActiveAudioId] = useState<string>("");
   const {isRunning} = props;
 
-  const t = useCallback((string: string) => {
-    const stringToRender = string === "CO2" ? <span>CO<sub>2</sub></span> : translations[string].string;
-    if (readAloudMode && "mp3" in translations[string]) {
-      const audio = new Audio(translations[string].mp3);
-
-      audio.addEventListener("playing", () => {
-        setIsAudioPlaying(true);
-        setActiveAudioId(string);
-      });
-
-      audio.addEventListener("ended", () => {
-        setIsAudioPlaying(false);
-        setActiveAudioId("");
-      });
-
-      const handlePlay = () => {
-        if (!isDisabled && !isAudioPlaying) {
-          audio.load();
-          audio.play();
-        }
-      };
-
-      const isActive = isAudioPlaying && activeAudioId === string;
-      const isDisabled = isRunning || (isAudioPlaying && activeAudioId !== string);
-
-      const classes = {
-        [css.readAloud]: true,
-        [css.active]: isActive,
-        [css.disabled]: isDisabled || isAudioPlaying
-      };
-
-      return (
-        <div onClick={handlePlay}>
-          <span className={clsx(classes)}>{stringToRender}</span>
-        </div>
-      );
-    } else {
-      return stringToRender;
-    }
-  }, [readAloudMode, isAudioPlaying, isRunning, activeAudioId]);
+  const t = (string: string) => {
+    return (
+      <Translation
+        readAloudMode={readAloudMode}
+        string={string}
+        isAudioPlaying={isAudioPlaying}
+        setIsAudioPlaying={setIsAudioPlaying}
+        isRunning={isRunning} />
+    );
+  };
 
   return {
     t,
