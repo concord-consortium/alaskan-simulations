80ecb6266fe798db8416f450f67dae72661826ad
pjanik <janikpiotrek@gmail.com>
2023-07-11 15:59:19 +0200

    chore: remove old FOSS translation helpers [PT-185575293]

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,5 +1,4 @@
 export * from "./utils/config";
-export * from "./translation/translate";
 
 export * from "./components/simulation-frame/simulation-frame";
 export * from "./components/simulation-frame/dialog";
@@ -12,7 +11,3 @@ export * from "./hooks/use-simulation-runner";
 export * from "./hooks/use-current";
 export * from "./hooks/use-interactive-state";
 export * from "./hooks/use-translation";
-
-// this exists to make translation import cleaner in the sims
-import { translate } from "./translation/translate";
-export const t = translate;
