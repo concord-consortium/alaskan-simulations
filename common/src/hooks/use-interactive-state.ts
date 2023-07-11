04529c394d913f00602586227fae28f5b055831a
pjanik <janikpiotrek@gmail.com>
2023-07-11 18:28:31 +0200

    chore: address PR comments [PT-185561938]

--- common/src/hooks/use-interactive-state.ts
+++ common/src/hooks/use-interactive-state.ts
@@ -1,12 +1,11 @@
 import { useCallback } from "react";
 import { useInteractiveState } from "@concord-consortium/lara-interactive-api";
-import { IInteractiveState } from "../../../plant-growth/src/types";
 
 export const useSaveInteractiveState = () => {
-  const { setInteractiveState } = useInteractiveState<IInteractiveState>();
+  const { setInteractiveState } = useInteractiveState<any>();
 
   const saveInteractiveState = useCallback((updates: any) => {
-    setInteractiveState((prevState) => {
+    setInteractiveState((prevState: any) => {
       return {
         answerType: "interactive_state",
         ...prevState,
