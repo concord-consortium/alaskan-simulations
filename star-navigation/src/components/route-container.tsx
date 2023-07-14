ec0607e35da49859e95ec2dd02cb9c4ca7a74529
lublagg <lvblagg@gmail.com>
2023-07-14 16:56:29 -0400

    Feat: circle representing angle auto-populates.

--- star-navigation/src/components/route-container.tsx
+++ star-navigation/src/components/route-container.tsx
@@ -13,6 +13,10 @@ interface LineProps {
   x1: number, y1: number, x2: number, y2: number
 }
 
+const angle1 = "angle1";
+const angle2 = "angle2";
+type WhichAngle = typeof angle1 | typeof angle2;
+
 export const RouteContainer: React.FC = () => {
   const [pointB, setPointB] = useState<{x: number, y: number}>({x: 105, y: 80});
 
@@ -30,18 +34,22 @@ export const RouteContainer: React.FC = () => {
     }
   };
 
+  const convertRadiansToDegrees = (rad: number) => {
+    return Math.round((rad) * 180/Math.PI);
+  }
 
-  const getAngle = (hypLength: number, whichAngle: "angle1" | "angle2") => {
-    const convertToDegrees = (num: number) => Math.round((num) * 180/Math.PI);
-    const otherSideLength = whichAngle === "angle1" ? pointB.x - pointA.x : pointC.x - pointB.x;
-    const angle = convertToDegrees(whichAngle === "angle1" ? Math.acos(otherSideLength / hypLength) : Math.asin(otherSideLength / hypLength));
-    let angleInDegrees: number;
-    if (whichAngle === "angle1") {
-      angleInDegrees = pointB.y > pointA.y ? 90 + angle : angleInDegrees = 90 - angle;
+
+  const getAngle = (hypLength: number, isFirstAngle: boolean) => {
+    const otherSideLength = isFirstAngle ? pointB.x - pointA.x : pointC.x - pointB.x;
+    const angleInRadians = isFirstAngle ? Math.acos(otherSideLength / hypLength) : Math.asin(otherSideLength / hypLength);
+    const angleInDegrees = convertRadiansToDegrees(angleInRadians);
+    let finalAngle: number;
+    if (isFirstAngle) {
+      finalAngle = pointB.y > pointA.y ? 90 + angleInDegrees : 90 - angleInDegrees;
     } else {
-      angleInDegrees = pointB.y < pointC.y ? 180 - angle : angle;
+      finalAngle = pointB.y < pointC.y ? 180 - angleInDegrees : angleInDegrees;
     }
-    return angleInDegrees;
+    return finalAngle;
   }
 
   const makeLine = (coords: LineProps) => {
@@ -51,15 +59,28 @@ export const RouteContainer: React.FC = () => {
     );
   };
 
-  const makeLineWithTextAndAngle = (coords: LineProps, whichAngle: "angle1" | "angle2") => {
+  const makeLineWithTextAndAngle = (coords: LineProps, whichAngle: WhichAngle) => {
     const {x1, y1, x2, y2} = coords;
-    const length = Math.round(Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2))));
-    const txtPos = {x: (x2+x1) / 2, y: ((y2+y1) / 2)}
+    const length = (Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2))));
+    const middleOfLine = {x: (x2+x1) / 2, y: ((y2+y1) / 2)}
+
+    const angle = getAngle(length, whichAngle === angle1);
+    const radius = ((x2-x1) * .75) > 70 ? 70 : (x2-x1) * .75;
+    const degTxtXPos = radius === 50 ? x1 + 50 : middleOfLine.x;
+    const clipPath = whichAngle === angle1 ? "cut-off-left" : "cut-off-right";
+
     return (
       <>
+      { /* create a polygon to clip the circle to only the parts we want to see */ }
+      <defs>
+        <clipPath id={clipPath}>
+          <polygon points={`${x1+1},${y1-1} ${x1 + 1},0 ${x2 + 1},0 ${x2},${y2} `}/>
+        </clipPath>
+      </defs>
         {makeLine(coords)}
-        <text x={txtPos.x} y={txtPos.y + 15} style={{stroke: "black", textAnchor: "middle", dominantBaseline:"middle"}}>{length}</text>
-        <text x={x1 + 20} y={txtPos.y - 15} style={{stroke: "black", textAnchor: "middle", dominantBaseline:"middle"}}>{getAngle(length, whichAngle)}</text>
+        <text x={middleOfLine.x} y={middleOfLine.y + 15} style={{stroke: "black", textAnchor: "middle", dominantBaseline:"middle"}}>{Math.round(length)}</text>
+        <circle r={radius} clipPath={`url(#${clipPath})`} cx={x1} cy={y1} className={css.angle}/>
+        <text x={degTxtXPos} y={middleOfLine.y - 15} style={{stroke: "black", textAnchor: "middle", dominantBaseline:"middle"}}>{angle}°</text>
       </>
     );
   };
@@ -71,9 +92,9 @@ export const RouteContainer: React.FC = () => {
         <div className={css.svgContainer}>
           <svg height={mapHeight} width={mapWidth}>
             {makeLine({x1: pointA.x, y1: 1, x2: pointA.x, y2: 149})}
-            {makeLineWithTextAndAngle({x1: pointA.x, y1: pointA.y, x2: pointB.x, y2: pointB.y}, "angle1")}
+            {makeLineWithTextAndAngle({x1: pointA.x, y1: pointA.y, x2: pointB.x, y2: pointB.y}, angle1)}
             {makeLine({x1: pointB.x, y1: 1, x2: pointB.x, y2: 149})}
-            {makeLineWithTextAndAngle({x1: pointB.x, y1: pointB.y, x2: pointC.x, y2: pointC.y}, "angle2")}
+            {makeLineWithTextAndAngle({x1: pointB.x, y1: pointB.y, x2: pointC.x, y2: pointC.y}, angle2)}
           </svg>
           <div className={css.draggableIcon}>
             <DraggableWrapper onDragMove={handleDragMove}>
