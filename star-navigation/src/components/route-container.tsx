705c4291258265fe185473c040ff2651a57002d5
lublagg <lvblagg@gmail.com>
2023-07-14 12:23:29 -0400

    Feat: angle of northern line to trip segment auto-populates.

--- star-navigation/src/components/route-container.tsx
+++ star-navigation/src/components/route-container.tsx
@@ -30,12 +30,39 @@ export const RouteContainer: React.FC = () => {
     }
   };
 
+
+  const getAngle = (hypLength: number, whichAngle: "angle1" | "angle2") => {
+    const convertToDegrees = (num: number) => Math.round((num) * 180/Math.PI);
+    const otherSideLength = whichAngle === "angle1" ? pointB.x - pointA.x : pointC.x - pointB.x;
+    const angle = convertToDegrees(whichAngle === "angle1" ? Math.acos(otherSideLength / hypLength) : Math.asin(otherSideLength / hypLength));
+    let angleInDegrees: number;
+    if (whichAngle === "angle1") {
+      angleInDegrees = pointB.y > pointA.y ? 90 + angle : angleInDegrees = 90 - angle;
+    } else {
+      angleInDegrees = pointB.y < pointC.y ? 180 - angle : angle;
+    }
+    return angleInDegrees;
+  }
+
   const makeLine = (coords: LineProps) => {
     const {x1, y1, x2, y2} = coords;
     return (
-      <line x1={x1} y1={y1} x2={x2} y2={y2} style={{stroke: "black", strokeWidth: 2}} />
-    )
-  }
+      <line x1={x1} y1={y1} x2={x2} y2={y2} style={{stroke: "black", strokeWidth: 2}}/>
+    );
+  };
+
+  const makeLineWithTextAndAngle = (coords: LineProps, whichAngle: "angle1" | "angle2") => {
+    const {x1, y1, x2, y2} = coords;
+    const length = Math.round(Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2))));
+    const txtPos = {x: (x2+x1) / 2, y: ((y2+y1) / 2)}
+    return (
+      <>
+        {makeLine(coords)}
+        <text x={txtPos.x} y={txtPos.y + 15} style={{stroke: "black", textAnchor: "middle", dominantBaseline:"middle"}}>{length}</text>
+        <text x={x1 + 20} y={txtPos.y - 15} style={{stroke: "black", textAnchor: "middle", dominantBaseline:"middle"}}>{getAngle(length, whichAngle)}</text>
+      </>
+    );
+  };
 
   return (
     <div className={css.rightPanelContainer}>
@@ -43,8 +70,10 @@ export const RouteContainer: React.FC = () => {
         <div className={css.mapBackground}/>
         <div className={css.svgContainer}>
           <svg height={mapHeight} width={mapWidth}>
-            {makeLine({x1: pointA.x, y1: pointA.y, x2: pointB.x, y2: pointB.y})}
-            {makeLine({x1: pointB.x, y1: pointB.y, x2: pointC.x, y2: pointC.y})}
+            {makeLine({x1: pointA.x, y1: 1, x2: pointA.x, y2: 149})}
+            {makeLineWithTextAndAngle({x1: pointA.x, y1: pointA.y, x2: pointB.x, y2: pointB.y}, "angle1")}
+            {makeLine({x1: pointB.x, y1: 1, x2: pointB.x, y2: 149})}
+            {makeLineWithTextAndAngle({x1: pointB.x, y1: pointB.y, x2: pointC.x, y2: pointC.y}, "angle2")}
           </svg>
           <div className={css.draggableIcon}>
             <DraggableWrapper onDragMove={handleDragMove}>
