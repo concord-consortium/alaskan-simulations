f173b9414aed9b6fc67aad3582bcf056a32843e9
pjanik <janikpiotrek@gmail.com>
2023-08-01 14:40:21 +0200

    feat: improve time circular slider keyboard handling [PT-185589463] [PT-185737637]

--- star-navigation/src/components/bottom-container/time-circular-input.tsx
+++ star-navigation/src/components/bottom-container/time-circular-input.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import { CircularInput, CircularThumb } from "react-circular-input";
 import { config } from "../../config";
 import { getDaylightTimes, validFractionalHour } from "../../utils/daylight-utils";
@@ -25,11 +25,11 @@ const hourToAngle = (hour: number) => hour * 360 / 24;
 export const TimeCircularInput: React.FC<IProps> = ({ value, month, day, onChange }) => {
   const { observerLat, observerLong } = config;
 
-  const handleChange = (newValue: number) => {
+  const handleChange = useCallback((newValue: number) => {
     onChange(newValue * 24);
-  };
+  }, [onChange]);
 
-  const handleKeyDown = (event: React.KeyboardEvent) => {
+  const handleKeyDown = useCallback((event: KeyboardEvent | React.KeyboardEvent) => {
     // Increase value by 1 minute when user presses right arrow or space
     if (event.key === "ArrowRight" || event.key === " ") {
       onChange(validFractionalHour(value + 1 / 60));
@@ -38,8 +38,10 @@ export const TimeCircularInput: React.FC<IProps> = ({ value, month, day, onChang
     else if (event.key === "ArrowLeft") {
       onChange(validFractionalHour(value - 1 / 60));
     }
+    // Important to stop propagation, otherwise CircularInput will handle the event and change the value using too big
+    // and not configurable step.
     event.stopPropagation();
-  };
+  }, [onChange, value]);
 
   const times = getDaylightTimes({ month, day, timeOfDay: value, lat: observerLat, long: observerLong });
   const sunrise = hourToAngle(times.sunrise);
@@ -47,21 +49,30 @@ export const TimeCircularInput: React.FC<IProps> = ({ value, month, day, onChang
   const nightStart = hourToAngle(times.nightStart);
   const nightEnd = hourToAngle(times.nightEnd);
 
+  // Set up keyboard handling globally. It should not conflict with any other component in this particular simulation,
+  // and it might be useful to fine tune the time when users are calculating journey duration.
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [handleKeyDown, onChange, value]);
+
   return (
+    // It's necessary to handle keydown on the whole component, so the event can be captured and propagation stopped
+    // before CircularInput handles it and changes the value using too big and not configurable step.
     <div className={css.timeCircularInput} onKeyDown={handleKeyDown}>
       <CircularInput
         value={value / 24} // value is always between [0, 1]
         onChange={handleChange}
         radius={RADIUS}
-        tabIndex={-1}
+        tabIndex={-1} // disable focus, as default keyboard handling is not working well (too big step, not configurable)
       >
         {/* Track */}
         <path className={css.nightTrack} d={describeArc(nightStart, nightEnd)} />
         <path className={css.daylightTrack} d={describeArc(sunrise, sunset)} />
         <path className={css.twilightTrack} d={describeArc(nightEnd, sunrise)} />
         <path className={css.twilightTrack} d={describeArc(sunset, nightStart)} />
-        {/* "Hours" */}
         {
+          // Hour marks
           HOUR_MARKS.map((hour) =>
             <circle
               key={hour}
@@ -79,6 +90,7 @@ export const TimeCircularInput: React.FC<IProps> = ({ value, month, day, onChang
         />
         {/* Center point, just style */}
         <circle className={css.center} cx={RADIUS} cy={RADIUS} r={5} />
+        {/* Make thumb focusable by setting tabIndex=0 */}
         <CircularThumb className={css.timeInputHandle} tabIndex={0} />
       </CircularInput>
     </div>
